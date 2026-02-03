85b80cb06297f7baf5879208bf83ddf2e1a40863
Marcello Perathoner <marcello@perathoner.de>
2026-02-03 17:03:24 +0100

    WIP

--- scripts/ci/runtime_dependencies.py
+++ scripts/ci/runtime_dependencies.py
@@ -70,25 +70,28 @@ def main():
         # 	@rpath/libjli.dylib (compatibility version 1.0.0, current version 1.0.0)
         # 	/System/Library/Frameworks/Foundation.framework/Versions/C/Foundation (compatibility version 300.0.0, current version 1856.105.0)
         # 	/usr/lib/libobjc.A.dylib (compatibility version 1.0.0, current version 228.0.0)
-        regex = re.compile(r"^\s+(.*dylib)\s")
+        regex = re.compile(r"^\s+(/.*dylib)\s")
     if platform.system() == "Windows":
         # https://learn.microsoft.com/en-us/cpp/build/reference/dependents?view=msvc-170
+        # https://github.com/actions/runner-images/blob/main/images/windows/Windows2025-Readme.md
         tool = glob.glob(
-            "C:\\Program Files\\Microsoft Visual Studio\\*\\Enterprise\\VC\\Tools\\MSVC\\*\\bin\\Hostx64\\x64\\dumpbin.exe"
+            r"C:\\Program Files\\Microsoft Visual Studio\\**\\DUMPBIN.EXE",
+            recursive=True,
         )
-        if len(tool) == 1:
-            print(f"Found: {tool}")
-            tool += ["/DEPENDENTS"]
+        if len(tool) > 0:
+            print(f"Found: {tool[0]}")
+            tool = [tool[0], "/DEPENDENTS"]
             regex = re.compile(r"^\s+(.*dll)$")
         # FIXME: find the path of the dlls using PATH
 
     libs = set()
 
     for filename in args.filenames:
-        with subprocess.Popen(
-            tool + [filename], stdout=subprocess.PIPE, encoding="utf-8"
-        ) as proc:
+        tool += [filename]
+        print(f"Running: {tool}")
+        with subprocess.Popen(tool, stdout=subprocess.PIPE, encoding="utf-8") as proc:
             for line in proc.stdout.readlines():
+                print(line)
                 m = regex.search(line)
                 if m:
                     libs.add(m.group(1))
