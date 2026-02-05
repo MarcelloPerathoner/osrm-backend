b8668ee3d5e616d08afa08dfa01a0dac2b8c149f
Marcello Perathoner <marcello@perathoner.de>
2026-02-05 08:30:51 +0100

    WIP

--- scripts/ci/runtime_dependencies.py
+++ scripts/ci/runtime_dependencies.py
@@ -43,18 +43,20 @@ def main():
 
     if platform.system() == "Linux":
         # ldd filename
-        tool = ["ldd"]
+        tool = "ldd"
         # <TAB>libboost_date_time.so.1.83.0 => /path/to/libboost_date_time.so.1.83.0 (0x00007fcf3276f000)
         regex = re.compile(r" => (.*) \(0x")
         # regex = re.compile(r"\s+(.*) => (.*) \(0x")
+        path = os.environ.get("LD_LIBRARY_PATH", "").split(":")
     if platform.system() == "Darwin":
         # otool -L libfoo.dylib
-        tool = ["otool", "-L"]
+        tool = "otool -L"
         # /home/me/lib/libjli.dylib:
         # 	@rpath/libjli.dylib (compatibility version 1.0.0, current version 1.0.0)
         # 	/System/Library/Frameworks/Foundation.framework/Versions/C/Foundation (compatibility version 300.0.0, current version 1856.105.0)
         # 	/usr/lib/libobjc.A.dylib (compatibility version 1.0.0, current version 228.0.0)
         regex = re.compile(r"^\s+(/.*dylib)\s")
+        path = os.environ.get("DYLD_LIBRARY_PATH", "").split(":")
     if platform.system() == "Windows":
         # https://learn.microsoft.com/en-us/cpp/build/reference/dependents?view=msvc-170
         # https://github.com/actions/runner-images/blob/main/images/windows/Windows2025-Readme.md
@@ -63,15 +65,18 @@ def main():
             recursive=True,
         )
         if len(tool) > 0:
-            tool = [tool[0], "/DEPENDENTS"]
+            tool = tool[0] + " /DEPENDENTS"
             regex = re.compile(r"^\s+(.*dll)$")
-        # FIXME: find the path of the dlls using PATH
+        else:
+            print("Cannot find dumpbin.exe")
+            exit(-1)
+        path = os.environ.get("PATH", "").split(":")
 
     def find_lib(lib: str) -> str:
         """Find the library in PATH"""
         if "/" in lib:
             return lib
-        for p in os.environ.get("PATH", "").split(":"):
+        for p in path:
             p = os.path.join(p, lib)
             if os.access(p, os.R_OK):
                 return p
@@ -80,18 +85,19 @@ def main():
     def gather(binaries):
         libs = []
         for bin in list(binaries):
-            with subprocess.Popen(
-                tool + [bin], stdout=subprocess.PIPE, encoding="utf-8"
-            ) as proc:
-                for line in proc.stdout.readlines():
-                    m = regex.search(line)
-                    if m:
-                        libs.append(find_lib(m.group(1)))
+            stdout = subprocess.check_output(
+                f"{tool} {bin}", shell=True, encoding="utf-8"
+            )
+            print(stdout)
+            for line in stdout.splitlines():
+                m = regex.search(line)
+                if m:
+                    libs.append(find_lib(m.group(1)))
         return libs
 
     for lib in gather(args.filenames):
         # be careful *not* to resolve symlinks! eg. libtbbmalloc.so.2 => libtbbmalloc.so.2.17
-        # path = os.path.relpath(path)
+        lib = os.path.normpath(lib)
         if args.grep:
             m = args.grep.search(lib)
             if m:
