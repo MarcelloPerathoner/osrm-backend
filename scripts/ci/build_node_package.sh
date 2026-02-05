0ffafdafeb5d318634cebe31112c8069397816af
Marcello Perathoner <marcello@perathoner.de>
2026-02-05 23:58:47 +0100

    WIP

--- scripts/ci/build_node_package.sh
+++ scripts/ci/build_node_package.sh
@@ -22,43 +22,44 @@ fi
 # copy files into BINDINGS
 
 source build/cmake.env
+cmake --install "$OSRM_BUILD_DIR" --component node_osrm -v
 
-cp package.json "$BUILD"
-cp src/nodejs/index.js "$BUILD/lib"
-cp "$OSRM_NODEJS_BUILD_DIR/$NODE_OSRM" "$BINDINGS"
-for n in components contract customize datastore extract partition routed ; do
-    cp -v "$OSRM_BUILD_DIR/osrm-$n${EXE:-}" "$BINDINGS"
-done
-
-# Copy dynamic library dependencies. Note: the RPATH inside node_osrm.node is not enough
-# because it is not valid for recursively loaded libraries: when boost_random imports
-# boost_system there is no RPATH in boost_random.
-if [[ "$CONAN_GENERATORS_DIR" != "" ]]; then
-    source "$CONAN_GENERATORS_DIR/conan-run-env.sh"
-    export LD_LIBRARY_PATH DYLD_LIBRARY_PATH PATH
-fi
-
-DEPS=`python scripts/ci/runtime_dependencies.py --grep "boost|bz2|tbb|osrm" "$OSRM_NODEJS_BUILD_DIR/$NODE_OSRM"
-echo "=== Dependencies ==="
-echo "$DEPS"
-echo "===================="
-
-# cp on macOS knows no -t, no -u
-echo "$DEPS" | xargs -I '{}' cp -v '{}' "$BINDINGS" || true
-
-case $(uname) in
-  Linux)
-    ldd "$BINDINGS/$NODE_OSRM"
-    chrpath --replace '$ORIGIN' "$BINDINGS/$NODE_OSRM"
-  ;;
-  Darwin)
-    otool -l "$BINDINGS/$NODE_OSRM"
-    otool -L "$BINDINGS/$NODE_OSRM"
-  ;;
-  Windows)
-    DUMPBIN /DEPENDENTS "$BINDINGS/$NODE_OSRM"
-  ;;
-esac
+# cp package.json "$BUILD"
+# cp src/nodejs/index.js "$BUILD/lib"
+# cp "$OSRM_NODEJS_BUILD_DIR/$NODE_OSRM" "$BINDINGS"
+# for n in components contract customize datastore extract partition routed ; do
+#     cp -v "$OSRM_BUILD_DIR/osrm-$n${EXE:-}" "$BINDINGS"
+# done
+#
+# # Copy dynamic library dependencies. Note: the RPATH inside node_osrm.node is not enough
+# # because it is not valid for recursively loaded libraries: when boost_random imports
+# # boost_system there is no RPATH in boost_random.
+# if [[ "$CONAN_GENERATORS_DIR" != "" ]]; then
+#     source "$CONAN_GENERATORS_DIR/conan-run-env.sh"
+#     export LD_LIBRARY_PATH DYLD_LIBRARY_PATH PATH
+# fi
+#
+# DEPS=`python scripts/ci/runtime_dependencies.py --grep "boost|bz2|tbb|osrm" "$OSRM_NODEJS_BUILD_DIR/$NODE_OSRM"
+# echo "=== Dependencies ==="
+# echo "$DEPS"
+# echo "===================="
+#
+# # cp on macOS knows no -t, no -u
+# echo "$DEPS" | xargs -I '{}' cp -v '{}' "$BINDINGS" || true
+#
+# case $(uname) in
+#   Linux)
+#     ldd "$BINDINGS/$NODE_OSRM"
+#     chrpath --replace '$ORIGIN' "$BINDINGS/$NODE_OSRM"
+#   ;;
+#   Darwin)
+#     otool -l "$BINDINGS/$NODE_OSRM"
+#     otool -L "$BINDINGS/$NODE_OSRM"
+#   ;;
+#   Windows)
+#     DUMPBIN /DEPENDENTS "$BINDINGS/$NODE_OSRM"
+#   ;;
+# esac
 
 # echo "dumping binary meta..."
 ./node_modules/.bin/node-pre-gyp reveal $NPM_FLAGS
